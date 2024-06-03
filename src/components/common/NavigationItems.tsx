85a01a202708241ccd2bae50609458682ffae844
Matej Poklemba <matejpoklemba@gmail.com>
2024-06-03 09:35:23 +0200

    Add footer to NavigationItems

--- src/components/common/NavigationItems.tsx
+++ src/components/common/NavigationItems.tsx
@@ -1,4 +1,4 @@
-import { Button, Stack } from "@mui/material";
+import { Button, Stack, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
 import ConnectWallet from "./ConnectWallet";
 
@@ -25,19 +25,23 @@ export default function NavigationItems({
   onClick,
 }: Props) {
   return (
-    <Stack sx={{ width: "100%", gap: 1, overflowY: "auto" }}>
-      {showWallet && <ConnectWallet />}
-      {items.map((item) => (
-        <Link to={item.href} key={item.href}>
-          <Button
-            variant="text"
-            sx={{ width: "100%", justifyContent: "start" }}
-            onClick={onClick}
-          >
-            {item.label}
-          </Button>
-        </Link>
-      ))}
-    </Stack>
+    <>
+      <Stack sx={{ width: "100%", gap: 1, overflowY: "auto" }}>
+        {showWallet && <ConnectWallet />}
+        {items.map((item) => (
+          <Link to={item.href} key={item.href}>
+            <Button
+              variant="text"
+              sx={{ width: "100%", justifyContent: "start" }}
+              onClick={onClick}
+            >
+              {item.label}
+            </Button>
+          </Link>
+        ))}
+      </Stack>
+      <Stack sx={{ flexGrow: 1 }} />
+      <Typography variant="body2">© 2022-2024 Paima Studios LTD.</Typography>
+    </>
   );
 }
