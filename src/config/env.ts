ce040efb767c68444eb02f2b89c2bf2fd16960de
Matej Poklemba <matejpoklemba@gmail.com>
2024-06-04 17:59:29 +0200

    Remove unused stuff from env

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,9 +1,7 @@
 import { bool, cleanEnv, json, str, url } from "envalid";
 
 const env = cleanEnv(process.env, {
-  REACT_APP_BLOCKFROST_PROJECT_ID: str(),
   REACT_APP_TESTNET: bool(),
-  REACT_APP_CARDANO_API_URL_BASE: url(),
   REACT_APP_WALLET_CONNECT_PROJECT_ID: str(),
   REACT_APP_WAGMI_TRANSPORTS: json<Record<string, string>>(),
   REACT_APP_ALCHEMY_API_KEYS: json<Record<string, string>>(),
