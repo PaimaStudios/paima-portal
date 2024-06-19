9ca197853d5707af5b84b7341c37330a96dcf158
Matej Poklemba <matejpoklemba@gmail.com>
2024-06-19 11:15:40 +0200

    Price chart updates
    Added volume to the bottom of the chart, as is tradition
    Time axis shows hours:minutes, also on mouse hover
    Cleaner grid lines (solid, darker gray color)

--- src/components/dex/PriceChart.tsx
+++ src/components/dex/PriceChart.tsx
@@ -11,6 +11,8 @@ export default function PriceChart() {
   const [chart, setChart] = useState<IChartApi>();
   const [chartSeries, setChartSeries] =
     useState<ReturnType<IChartApi["addCandlestickSeries"]>>();
+  const [volumeSeries, setVolumeSeries] =
+    useState<ReturnType<IChartApi["addHistogramSeries"]>>();
   const { width = 0, height = 0 } = useResizeObserver({
     ref: chartContainerRef,
   });
@@ -26,27 +28,57 @@ export default function PriceChart() {
         textColor: theme.palette.text.primary,
       },
       grid: {
-        horzLines: { style: LineStyle.SparseDotted },
-        vertLines: { style: LineStyle.SparseDotted },
+        horzLines: { style: LineStyle.Solid, color: theme.palette.grey[900] },
+        vertLines: { style: LineStyle.Solid, color: theme.palette.grey[900] },
       },
       rightPriceScale: {
         borderColor: "transparent",
       },
       timeScale: {
         borderColor: "transparent",
+        timeVisible: true,
+        secondsVisible: false,
       },
     });
     setChart(chart);
     const candleSeries = chart.addCandlestickSeries({
       priceFormat: { minMove: 0.00000001 },
     });
+    candleSeries.priceScale().applyOptions({
+      scaleMargins: {
+        top: 0.1, // highest point of the series will be 10% away from the top
+        bottom: 0.4, // lowest point will be 40% away from the bottom
+      },
+    });
     setChartSeries(candleSeries);
+    const histogramSeries = chart.addHistogramSeries({
+      priceFormat: {
+        type: "volume",
+      },
+      priceScaleId: "", // set as an overlay by setting a blank priceScaleId
+    });
+    histogramSeries.priceScale().applyOptions({
+      // set the positioning of the volume series
+      scaleMargins: {
+        top: 0.7, // highest point of the series will be 70% away from the top
+        bottom: 0,
+      },
+    });
+    setVolumeSeries(histogramSeries);
   }, [data]);
 
   useEffect(() => {
-    if (!data || !chartSeries) return;
+    if (!data || !chartSeries || !volumeSeries) return;
     if (chartSeries.data.length === 0) {
       chartSeries.setData(data.data as any);
+      volumeSeries.setData(
+        data.data.map((d) => ({
+          ...d,
+          value: d.volumeFrom,
+          color: d.close < d.open ? "#ef5350" : "#26a69a",
+        })) as any,
+      );
+      chart?.timeScale().fitContent();
     } else {
       const rawLastData = data.data[data.data.length - 1];
       const formattedData = {
@@ -54,8 +86,13 @@ export default function PriceChart() {
         time: new Date(rawLastData.time).toISOString(),
       };
       chartSeries.update(formattedData);
+      volumeSeries.update({
+        ...formattedData,
+        value: formattedData.volumeFrom,
+        color: formattedData.close < formattedData.open ? "#ef5350" : "#26a69a",
+      });
     }
-  }, [data, chartSeries]);
+  }, [data, chartSeries, volumeSeries]);
 
   useEffect(() => {
     if (!chart) return;
