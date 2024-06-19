8273b28bdd46cbbc66f14e75c498c8882e80a189
Matej Poklemba <matejpoklemba@gmail.com>
2024-06-19 09:35:51 +0200

    Use api url based on testnet flag

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,3 +1,7 @@
+import env from "./env";
+
 export const gamesApi: Record<string, string> = {
-  tarochi: "https://tarochi-backend-testnet-c1.paimastudios.com",
+  tarochi: env.REACT_APP_TESTNET
+    ? "https://tarochi-backend-testnet-c1.paimastudios.com"
+    : "https://",
 } as const;
