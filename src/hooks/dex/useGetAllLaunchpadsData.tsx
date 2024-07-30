dd8222e28a939c7e0ffe17945f95f17d4baccc19
Matej Poklemba <matejpoklemba@gmail.com>
2024-07-30 11:44:25 +0200

    Update launchpads data

--- src/hooks/dex/useGetAllLaunchpadsData.tsx
+++ src/hooks/dex/useGetAllLaunchpadsData.tsx
@@ -2,39 +2,114 @@ import { useQuery } from "@tanstack/react-query";
 import { QueryKeys } from "@utils/queryKeys";
 import axios from "axios";
 import env from "@config/env";
+import { ZERO_ADDRESS } from "@utils/constants";
 
 type CommonItemProps = {
-  id: string;
+  /**
+   * Item ID that will be also emitted in the events
+   */
+  id: `${number}`;
+  /**
+   * Item name to be displayed on the frontend
+   */
   name: string;
+  /**
+   * Item description displayed in the frontend
+   */
   description: string;
+  /**
+   * Optional URL to the image displayed in the frontend
+   */
   image?: string;
 };
 
 type StandardItem = CommonItemProps & {
-  // Map of token address to price of the item
+  /**
+   * Map of payment token identifier (address) to price of the item
+   */
   prices: Record<string, string>;
-  // Referral discount override to the price of the item, expressed in basis points
+  /**
+   * Optional override of the referral discount to the price of the item, expressed in basis points
+   */
   referralDiscountBps?: number;
 };
 
 type FreeRewardItem = CommonItemProps & {
-  // Map of token address to amount per which the item is able to be claimed for free
+  /**
+   * Map of payment token identifier (address) to amount per which the item is able to be claimed for free
+   */
   freeAt: Record<string, string>;
 };
 
 type ItemType = StandardItem | FreeRewardItem;
 
 export type LaunchpadData = {
+  /**
+   * Launchpad ID, preferably its contract address
+   */
   id: string;
+  /**
+   * Launchpad name to be displayed on the frontend
+   */
   name: string;
-  // URL-friendly slug of the name
+  /**
+   * URL-friendly slug of the name
+   */
   slug: string;
-  // Game description displayed in the launchpads list
+  /**
+   * Game description displayed in the launchpads list
+   */
   description: string;
+  /**
+   * URL to the image displayed in the launchpads list
+   */
   image?: string;
+  /**
+   * List of items available for purchase in the launchpad
+   */
   items: ItemType[];
-  // Default referral discount to the prices of items, expressed in basis points
+  /**
+   * UNIX timestamp of the start of the whitelist sale, in seconds
+   */
+  timestampStartWhitelistSale: number;
+  /**
+   * UNIX timestamp of the start of the public sale, in seconds
+   */
+  timestampStartPublicSale: number;
+  /**
+   * UNIX timestamp of the end of the sale, in seconds
+   */
+  timestampEndSale: number;
+  /**
+   * Default referral discount to the prices of items, expressed in basis points
+   */
   referralDiscountBps?: number;
+  /**
+   * List of curated packages of items (basically just a shortcut to add multiple items to the cart)
+   */
+  curatedPackages?: {
+    /**
+     * Package name to be displayed on the frontend
+     */
+    name: string;
+    /**
+     * Optional package description displayed in the frontend, if omitted the enumeration of package items will be displayed
+     */
+    description?: string;
+    /**
+     * List of items that are part of the package
+     */
+    items: {
+      /**
+       * Item ID corresponding to the `items` array
+       */
+      id: `${number}`;
+      /**
+       * Quantity of the item in the package
+       */
+      quantity: number;
+    }[];
+  }[];
 };
 
 type LaunchpadsResponse = {
@@ -49,6 +124,7 @@ export default function useGetAllLaunchpadsData() {
       //   `${env.REACT_APP_LAUNCHPAD_BACKEND_URL}/launchpads`,
       // );
 
+      const MOCK_USDC = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
       return [
         {
           id: "0xd8058efe0198ae9dD7D563e1b4938Dcbc86A1F81",
@@ -63,8 +139,8 @@ export default function useGetAllLaunchpadsData() {
               name: "Item A",
               description: "Description of Item A",
               prices: {
-                "0x0000000000000000000000000000000000000000": "10000000000000",
-                "0xusdc": "34681800000000000",
+                [ZERO_ADDRESS]: "10000000000000",
+                [MOCK_USDC]: "34681800000000000",
               },
               referralDiscountBps: 500,
             },
@@ -73,8 +149,8 @@ export default function useGetAllLaunchpadsData() {
               name: "Item B",
               description: "Description of Item B",
               prices: {
-                "0x0000000000000000000000000000000000000000": "20000000000000",
-                "0xusdc": "69363600000000000",
+                [ZERO_ADDRESS]: "20000000000000",
+                [MOCK_USDC]: "69363600000000000",
               },
             },
             {
@@ -82,9 +158,41 @@ export default function useGetAllLaunchpadsData() {
               name: "Free Item X",
               description: "Description of Free Item X",
               freeAt: {
-                "0x0000000000000000000000000000000000000000": "100000000000000",
-                "0xusdc": "346818000000000000",
+                [ZERO_ADDRESS]: "100000000000000",
+                [MOCK_USDC]: "346818000000000000",
               },
+            },
+          ],
+          timestampStartWhitelistSale: 1724743540,
+          timestampStartPublicSale: 1724829940,
+          timestampEndSale: 1724916340,
+          curatedPackages: [
+            {
+              name: "Package 1",
+              description: "Description of Package 1",
+              items: [
+                {
+                  id: "1",
+                  quantity: 1,
+                },
+                {
+                  id: "2",
+                  quantity: 1,
+                },
+              ],
+            },
+            {
+              name: "VIP Package",
+              items: [
+                {
+                  id: "1",
+                  quantity: 10,
+                },
+                {
+                  id: "100",
+                  quantity: 1,
+                },
+              ],
             },
           ],
         },
@@ -100,8 +208,8 @@ export default function useGetAllLaunchpadsData() {
               name: "Item A",
               description: "Description of Item A",
               prices: {
-                "0x0000000000000000000000000000000000000000": "10000000000000",
-                "0xusdc": "34681800000000000",
+                [ZERO_ADDRESS]: "10000000000000",
+                [MOCK_USDC]: "34681800000000000",
               },
             },
             {
@@ -109,11 +217,14 @@ export default function useGetAllLaunchpadsData() {
               name: "Item B",
               description: "Description of Item B",
               prices: {
-                "0x0000000000000000000000000000000000000000": "20000000000000",
-                "0xusdc": "69363600000000000",
+                [ZERO_ADDRESS]: "20000000000000",
+                [MOCK_USDC]: "69363600000000000",
               },
             },
           ],
+          timestampStartWhitelistSale: 1724743540,
+          timestampStartPublicSale: 1724829940,
+          timestampEndSale: 1724916340,
         },
       ];
 
