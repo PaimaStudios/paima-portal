fc670ddb17fd86b118daa0b1dbc37f395391bdb7
Matej Poklemba <matejpoklemba@gmail.com>
2024-08-08 16:07:39 +0200

    Conditionally show the info about referrer reward

--- src/components/launchpad/LaunchpadPurchaseSuccessDialog.tsx
+++ src/components/launchpad/LaunchpadPurchaseSuccessDialog.tsx
@@ -3,9 +3,12 @@ import { CloseIcon } from "@components/icons/GeneralIcons";
 import useConnectWallet from "@hooks/useConnectWallet";
 import { useEffect, useRef } from "react";
 
-type Props = { txHash?: string };
+type Props = { txHash?: string; hasReferralRewardSharing?: boolean };
 
-export default function LaunchpadPurchaseSuccessDialog({ txHash }: Props) {
+export default function LaunchpadPurchaseSuccessDialog({
+  txHash,
+  hasReferralRewardSharing,
+}: Props) {
   const { address } = useConnectWallet();
   const dialogRef = useRef<HTMLDialogElement>(null);
 
@@ -25,7 +28,7 @@ export default function LaunchpadPurchaseSuccessDialog({ txHash }: Props) {
       <div className="w-full p-4 bg-gray-1100 rounded-xl border-[1px] border-gray-600 text-white">
         <div className="flex flex-col gap-4">
           <div className="flex justify-between items-center">
-            <p className="text-lg font-semibold">Purchase successful!</p>
+            <p className="text-lg font-semibold">Purchase order sent!</p>
             <button
               id="closeModalButtonTop"
               className="h-8 w-8"
@@ -41,14 +44,20 @@ export default function LaunchpadPurchaseSuccessDialog({ txHash }: Props) {
             <p className="break-words">{txHash}</p>
           </div>
           <div className="flex flex-col">
-            <p>You have successfully purchased the selected items.</p>
+            <p>
+              You have successfully sent an order to purchase the selected
+              items.
+            </p>
             <p>They will be delivered to your wallet at a later date.</p>
           </div>
 
           <div className="flex gap-4 items-center justify-between">
             <p>
-              Refer your friends to this launchpad! They'll get a discount and
-              you'll get a portion of their purchase.
+              Refer your friends to this launchpad! If they use your referral
+              code, they'll get a discount
+              {hasReferralRewardSharing &&
+                ` and you'll get a portion of their purchase`}
+              .
             </p>
             <CopyButton
               buttonProps={{ additionalClasses: "whitespace-nowrap" }}
